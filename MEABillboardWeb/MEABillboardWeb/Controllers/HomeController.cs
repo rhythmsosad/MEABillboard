c420361ee6f04fd1b0668a5320c6a32170c23b27
rhythmsosad <rhythmsosad@gmail.com>
2015-02-25 14:05:03 +0700

    Add Test method

--- MEABillboardWeb/MEABillboardWeb/Controllers/HomeController.cs
+++ MEABillboardWeb/MEABillboardWeb/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace MEABillboardWeb.Controllers
         {
             return View();
         }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
     }
 }
