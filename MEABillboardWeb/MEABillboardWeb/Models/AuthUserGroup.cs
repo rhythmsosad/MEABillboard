34a903705699266b5ac2a96998a78ef68aab31ff
rhythmsosad <rhythmsosad@gmail.com>
2015-03-09 14:34:04 +0700

    Add AuthUser, AuthUserGroup Model

---
+++ MEABillboardWeb/MEABillboardWeb/Models/AuthUserGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MEABillboardWeb.Models
+{
+    public class AuthUserGroup
+    {
+        public AuthUserGroup()
+        {
+
+        }
+        
+        //GUID
+        //FN_NAME
+        //NAME
+        //USER_CNT
+        //ALLNAMELIST
+        //NAMELIST
+        //FUNCTIONLIST
+        //FUNCTION
+        //ALLNAME
+        //SNAME
+
+        public int GroupId { get; set; }
+        public string FunctionName { get; set; }
+        public string UserGroupName { get; set; }
+        public int UserCount { get; set; }
+        public string AllNameList { get; set; }
+        public string NameList { get; set; }
+        public string FunctionList { get; set; }
+        public string Function { get; set; }
+        public string AllName { get; set; }
+        public string UserInGroup { get; set; }
+    }
+}
---
+++ MEABillboardWeb/MEABillboardWeb/Models/MEABillboardContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MEABillboardWeb.Models
+{
+    public class MEABillboardContext : DbContext
+    {
+        public MEABillboardContext()
+            : base()
+        {
+
+        }
+
+        public DbSet<AuthUser> AuthUsers { get; set; }
+        public DbSet<AuthUserGroup> AuthUserGroups { get; set; }
+    }
+}
