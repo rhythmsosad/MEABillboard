2e52e6533cc13add28f19e0ce6104b3436b76dc8
oop898 <oop898@hotmail.com>
2015-03-09 14:50:06 +0700

    AdminFunction #2

--- MEABillboardWeb/MEABillboardWeb/Areas/Admin/Models/AdminFunction.cs
+++ MEABillboardWeb/MEABillboardWeb/Areas/Admin/Models/AdminFunction.cs
@@ -7,5 +7,20 @@ namespace MEABillboardWeb.Areas.Admin.Models
 {
     public class AdminFunction
     {
+        public AdminFunctionItem Find(int id)
+        {
+            return new AdminFunctionItem();
+        }
     }
+
+    public class AdminFunctionItem
+    {
+        public string Code { get; set; }
+        public string Menu { get; set; }
+        public string FileDir { get; set; }
+        public string Icon { get; set; }
+        public string Order { get; set; }
+        public string Status { get; set; }
+    }
+
 }
