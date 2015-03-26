28dcfd8cbbf51322ef8c29f1c59fe38ea07e7970
sniper01 <pop_sc32@hotmail.com>
2015-03-26 13:29:48 +0700

    Add news info view

--- MEABillboardWeb/MEABillboardWeb/Controllers/NewsController.cs
+++ MEABillboardWeb/MEABillboardWeb/Controllers/NewsController.cs
@@ -13,5 +13,10 @@ namespace MEABillboardWeb.Controllers
         {
             return View();
         }
+
+        public ActionResult Info()
+        {
+            return View();
+        }
     }
 }
