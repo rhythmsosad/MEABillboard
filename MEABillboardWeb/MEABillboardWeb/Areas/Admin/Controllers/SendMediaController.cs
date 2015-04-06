fe6012c7dc643f95a59fa4b1770c608a6613bae5
oop898 <oop898@gmail.com>
2015-04-06 12:26:09 +0700

    create SendMeadia

---
+++ MEABillboardWeb/MEABillboardWeb/Areas/Admin/Controllers/SendMediaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MEABillboardWeb.Areas.Admin.Controllers
+{
+    public class SendMediaController : Controller
+    {
+        // GET: Admin/SendMedia
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
