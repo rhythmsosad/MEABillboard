a392a3e6cee52570f2fcbd0fac440c78d61a9dcb
sniper01 <pop_sc32@hotmail.com>
2015-04-07 09:09:21 +0700

    Add Admin Contract management

---
+++ MEABillboardWeb/MEABillboardWeb/Areas/Admin/Controllers/ContractController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MEABillboardWeb.Areas.Admin.Controllers
+{
+    public class ContractController : Controller
+    {
+        // GET: Admin/Contract
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/Contract
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
