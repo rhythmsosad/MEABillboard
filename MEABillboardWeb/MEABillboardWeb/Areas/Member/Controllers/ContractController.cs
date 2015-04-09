7592e85b44d4f7f8f630861e2ce3709ac08cf778
sniper01 <pop_sc32@hotmail.com>
2015-04-09 11:56:20 +0700

    add areas

---
+++ MEABillboardWeb/MEABillboardWeb/Areas/Member/Controllers/ContractController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MEABillboardWeb.Areas.Member.Controllers
+{
+    public class ContractController : Controller
+    {
+        // GET: Member/Contract
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+    }
+}
---
+++ MEABillboardWeb/MEABillboardWeb/Areas/Member/Controllers/MediaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MEABillboardWeb.Areas.Member.Controllers
+{
+    public class MediaController : Controller
+    {
+        // GET: Member/Media
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+        public ActionResult SendMedia()
+        {
+            return View();
+        }
+
+        
+    }
+}
